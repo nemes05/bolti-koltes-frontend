48431f85169fac84560571c05cc05468bd0986eb
nemes05 <nemes.barnabas020513@gmail.com>
2023-09-27 15:45:10 +0200

    ⬆️ Upgrades project dependencies

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
 import { PaperProvider } from 'react-native-paper'
+
+import MainScreen from './src/Components/MainScreen'
 
 export default function App() {
     return (
         <PaperProvider>
-            <View style={styles.container}>
-                <Text>Initialized...</Text>
-            </View>
+            <MainScreen />
         </PaperProvider>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
