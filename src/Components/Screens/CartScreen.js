0cdc7cc6fc74fc4fe4407a62b4edeb1c15be6e85
nemes05 <nemes.barnabas020513@gmail.com>
2023-09-27 18:14:05 +0200

    💄 Updates screens

--- src/Components/Screens/CartScreen.js
+++ src/Components/Screens/CartScreen.js
@@ -1,7 +1,18 @@
+import { View } from 'react-native'
 import { Text } from 'react-native-paper'
 
 const CartScreen = () => {
-    return <Text>Cart</Text>
+    return (
+        <View
+            style={{
+                alignItems: 'center',
+                height: 100,
+                padding: 20,
+            }}
+        >
+            <Text>Cart</Text>
+        </View>
+    )
 }
 
 export default CartScreen
--- src/Components/Screens/ListScreen.js
+++ src/Components/Screens/ListScreen.js
@@ -1,9 +1,18 @@
+import { View } from 'react-native'
 import { Text } from 'react-native-paper'
 
 const ListScreen = () => {
     return (
         <>
-            <Text>Lista</Text>
+            <View
+                style={{
+                    alignItems: 'center',
+                    height: 100,
+                    padding: 20,
+                }}
+            >
+                <Text>Lista</Text>
+            </View>
         </>
     )
 }
