4cbdfc9ac67a5251f00d0e2d256889070f497d29
nemes05 <nemes.barnabas020513@gmail.com>
2023-09-29 10:57:11 +0200

    ✨ Configures basic routing

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { PaperProvider } from 'react-native-paper'
 
 import MainScreen from './src/Screens/MainScreen'
 
 export default function App() {
+    const Stack = createNativeStackNavigator()
+
     return (
-        <PaperProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-                <MainScreen />
-            </GestureHandlerRootView>
-        </PaperProvider>
+        <NavigationContainer>
+            <PaperProvider>
+                <GestureHandlerRootView style={{ flex: 1 }}>
+                    <Stack.Navigator screenOptions={{ headerShown: false }}>
+                        <Stack.Screen
+                            name="main"
+                            component={MainScreen}
+                            style={{ flex: 1 }}
+                        />
+                    </Stack.Navigator>
+                </GestureHandlerRootView>
+            </PaperProvider>
+        </NavigationContainer>
     )
 }
