be5cf0d14f079273fe0495992a1ef3e188d14b11
nemes05 <nemes.barnabas020513@gmail.com>
2023-10-05 14:21:59 +0200

    ✨  Adds api connection

--- App.js
+++ App.js
@@ -3,22 +3,25 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { PaperProvider } from 'react-native-paper'
 
-import MainScreen from './src/Screens/MainScreen'
-import ScanScreen from './src/Screens/ScanScreen'
+import MainScreen from './src/Components/Screens/MainScreen'
+import ScanScreen from './src/Components/Screens/ScanScreen'
+import ApiProvider from './src/api/ApiProvider'
 
 export default function App() {
     const Stack = createNativeStackNavigator()
 
     return (
-        <NavigationContainer>
-            <PaperProvider>
-                <GestureHandlerRootView style={{ flex: 1 }}>
-                    <Stack.Navigator screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name="main" component={MainScreen} />
-                        <Stack.Screen name="scan" component={ScanScreen} />
-                    </Stack.Navigator>
-                </GestureHandlerRootView>
-            </PaperProvider>
-        </NavigationContainer>
+        <ApiProvider>
+            <NavigationContainer>
+                <PaperProvider>
+                    <GestureHandlerRootView style={{ flex: 1 }}>
+                        <Stack.Navigator screenOptions={{ headerShown: false }}>
+                            <Stack.Screen name="main" component={MainScreen} />
+                            <Stack.Screen name="scan" component={ScanScreen} />
+                        </Stack.Navigator>
+                    </GestureHandlerRootView>
+                </PaperProvider>
+            </NavigationContainer>
+        </ApiProvider>
     )
 }
