ba5b067b310f5ec8df45fd503cd1e63996f91e33
nemes05 <nemes.barnabas020513@gmail.com>
2023-11-01 12:42:16 +0100

    💄 Creates reusable modal for errors

---
+++ src/Components/UI/ErrorModal.js
@@ -0,0 +1,62 @@
+import { View, StyleSheet } from 'react-native'
+import { Card, Text, Button, Modal } from 'react-native-paper'
+
+const ErrorModal = ({ message, buttonText, visible, onDismiss, onPress }) => {
+    return (
+        <Modal
+            visible={visible}
+            onDismiss={(data) => {
+                onDismiss(data)
+            }}
+        >
+            <View style={styles.modalcardcontainer}>
+                <Card style={styles.modalcard}>
+                    <Card.Content>
+                        <Text style={styles.cardtext} variant="headlineSmall">
+                            {message}
+                        </Text>
+                    </Card.Content>
+                    <Card.Actions>
+                        <View style={styles.actioncontainer}>
+                            <Button
+                                style={styles.modalbutton}
+                                mode="outlined"
+                                onPress={() => {
+                                    onPress()
+                                }}
+                            >
+                                {buttonText}
+                            </Button>
+                        </View>
+                    </Card.Actions>
+                </Card>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    modalcardcontainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalcard: {
+        width: '90%',
+        padding: 15,
+    },
+    cardtext: {
+        textAlign: 'center',
+        margin: 10,
+    },
+    actioncontainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalbutton: {
+        margin: 10,
+    },
+})
+
+export default ErrorModal
