10207038c28475b8317b1c511f8e3473fe741ee3
nemes05 <nemes.barnabas020513@gmail.com>
2023-11-01 09:26:46 +0100

    🩹 Fixes loading text wrap

--- src/Components/UI/LoadIndicator.js
+++ src/Components/UI/LoadIndicator.js
@@ -2,24 +2,28 @@
  * A component for showing loading state
  * @param {string}  title   The text that should appear during the loading.
  */
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { ActivityIndicator, Text } from 'react-native-paper'
 
 const LoadIndicator = ({ title }) => {
     return (
-        <>
+        <View style={styles.container}>
             <ActivityIndicator animating size="large" />
             <Text style={styles.loadingtext} variant="labelLarge">
                 {title}
             </Text>
-        </>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+    },
     loadingtext: {
         textAlign: 'center',
-        flexWrap: 'nowrap',
         margin: 20,
     },
 })
