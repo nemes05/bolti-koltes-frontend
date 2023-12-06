4540c9bdf236df17cbb028688441926b8e362cfd
nemes05 <nemes.barnabas020513@gmail.com>
2023-12-06 14:27:01 +0100

    🥅 Adds error handling

--- src/Components/Screens/FavouritesScreen.js
+++ src/Components/Screens/FavouritesScreen.js
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from 'react'
 import { View, FlatList, StyleSheet } from 'react-native'
-import { IconButton, Text } from 'react-native-paper'
+import { IconButton, Portal, Text } from 'react-native-paper'
 
 import ApiContext from '../../Contexts/api/api-context'
 import PreferencesContext from '../../Contexts/preferences/preferences-context'
 import TopNavBar from '../Navigation/TopNavBar'
+import ErrorModal from '../UI/ErrorModal'
 import FavouriteProduct from '../UI/Product/Favourite/FavouriteProduct'
 import SimplifiedFavouriteProduct from '../UI/Product/Favourite/SimplifiedFavouriteProduct'
 
@@ -16,15 +17,39 @@ const FavouritesScreen = ({ navigation }) => {
     const preferences = useContext(PreferencesContext)
     const api = useContext(ApiContext)
     const [favourites, setFavourites] = useState([])
+    const [error, setError] = useState({ err: false, msg: '' })
 
     const getFavourites = async () => {
-        const favourites = await api.getFavourites()
-        setFavourites(favourites)
+        try {
+            const favourites = await api.getFavourites()
+            setFavourites(favourites)
+        } catch (err) {
+            setError({ err: true, msg: err.message })
+        }
     }
 
     useEffect(() => {
         if (api.userStatus) getFavourites()
     }, [])
+
+    if (error.err) {
+        return (
+            <Portal>
+                <ErrorModal
+                    message={error.msg}
+                    buttonText="Vissza"
+                    visible={error.err}
+                    dismissable
+                    onDismiss={() => {
+                        setError({ err: false, msg: '' })
+                    }}
+                    onButtonPress={() => {
+                        setError({ err: false, msg: '' })
+                    }}
+                />
+            </Portal>
+        )
+    }
 
     if (!api.userStatus) {
         return (
